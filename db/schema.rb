2b1b9ea06312c018e4448796af9c0a23f762ed3b
Jordan Kline <jordan.kline@gmail.com>
2018-02-09 22:19:53 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209201940) do
+ActiveRecord::Schema.define(version: 20180209221633) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.string "content"
-    t.string "author_name"
+    t.integer "author_id"
   end
 
 end
